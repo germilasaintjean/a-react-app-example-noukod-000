cbac359842d7fe0cdfa86b782dedd38cc2c5d97c
Saint-Jean Germila <germila@noukod.com>
2019-04-22 15:30:06 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
 
         </header>
-  
+
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.
           JSX lets us include JavaScript functions right along with the HTML, and also
